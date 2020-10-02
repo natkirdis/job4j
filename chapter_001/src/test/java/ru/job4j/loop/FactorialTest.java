5092e689814051f53a614e824372aeeef3efb2a0
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-10-02 08:16:42 +0300

    Создать программу, вычисляющую факториал.[#188653]

---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int rsl = Factorial.calc(5);
+        int expected = 120;
+        assertThat(rsl, Matchers.is(expected));
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int rsl = Factorial.calc(0);
+        int expected = 1;
+        assertThat(rsl, Matchers.is(expected));
+    }
+}
