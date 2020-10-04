7797839d3febec97bf9ac927755c61ab5bb6c1cf
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-10-04 20:58:36 +0300

    Слово заканчивается на ...[#188674]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int index = 1; index < post.length ; index++) {
+            if (word[word.length - index] != post[post.length - index]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
