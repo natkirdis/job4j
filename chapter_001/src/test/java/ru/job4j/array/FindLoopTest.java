312f5802bfe990975121915f98e563111d7ae84d
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-10-05 13:27:00 +0300

    Поиск индекса в диапазоне.[#188675]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,6 +15,7 @@ public class FindLoopTest {
         int expect = 0;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenArrayHasNo5ThenMinus1() {
         int input[] = {7, 4, 3, 2};
@@ -28,6 +30,46 @@ public class FindLoopTest {
         int value = 5;
         int result = FindLoop.indexOf(input, value);
         int expect = 3;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFind3() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas6LastDiaposonSimbolThen3() {
+        int[] input = new int[]{5, 2, 6, 2, 4};
+        int value = 6;
+        int start = 0;
+        int finish = 2;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas6FirstDiaposonSimbolThen3() {
+        int[] input = new int[]{5, 2, 6, 2, 4};
+        int value = 6;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHasNoDiaposonSimbolThenMinus1() {
+        int[] input = new int[]{5, 2, 6, 2, 4};
+        int value = 5;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
         assertThat(result, is(expect));
     }
 }
