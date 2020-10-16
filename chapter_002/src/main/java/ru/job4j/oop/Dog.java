dfbfea87c6f81b5cdf0ef16ea9f1450d1be41738
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-10-16 19:25:15 +0300

    Создание объекта[#188792]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharic = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
