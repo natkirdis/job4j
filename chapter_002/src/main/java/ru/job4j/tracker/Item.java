a7434ded98b74c38c984a5cdec6be38b6d3b3c52
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-10-17 21:09:33 +0300

    Перегрузка конструктора.[#188805]

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,14 +1,19 @@
 package ru.job4j.tracker;
 
 public class Item {
+    int id;
+    String name;
 
     public Item() {
     }
 
     public Item(String name) {
+        this.name = name;
     }
 
     public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public static void main(String[] args) {
