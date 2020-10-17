2a246fab03aed4ff69342d1cccc8324a2e398192
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-10-17 10:46:35 +0300

    Состояние объекта.[#188798]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery();
+        Battery second = new Battery();
+        first.load = 5;
+        second.load = 10;
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
+}
