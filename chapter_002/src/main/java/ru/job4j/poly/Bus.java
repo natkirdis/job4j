db0b594bd1e57df9bc6f330cc0a59b86111d7fff
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-10-22 23:00:22 +0300

    Интерфейс Input[#188769]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void passenger(int number) {
+
+    }
+
+    @Override
+    public int refuel(int liters) {
+        return 0;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    void move();
+
+    void passenger(int number);
+
+    int refuel(int amount);
+}
