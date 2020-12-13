10a18f58842f8487e6a91fbea4c5af4d0554799b
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-12-13 20:23:45 +0300

    1. Телефонный справочник на базе ArrayList[#188735]

---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,89 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("ntev");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("34");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Bry");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindOnePerson() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Natalia", "Kirdis", "9390044", "Saint-Petersburg")
+        );
+        ArrayList<Person> persons = phones.find("Natalia");
+        assertThat(persons.get(0).getSurname(), is("Kirdis"));
+    }
+
+    @Test
+    public void whenFindTwoPersons() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Petr", "Kirdis", "9390044", "Saint-Petersburg")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+        assertThat(persons.get(1).getSurname(), is("Kirdis"));
+    }
+
+    @Test
+    public void whenFindNoPersons() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Petr", "Kirdis", "9390044", "Saint-Petersburg")
+        );
+        ArrayList<Person> persons = phones.find("Maria");
+        assertThat(persons.size(), is(0));
+    }
+}
